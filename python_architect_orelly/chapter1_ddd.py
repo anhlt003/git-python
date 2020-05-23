f6b81fe278dac4d04472d23f9a0e6b3a6c4b0260
anhlt003 <anhlt003@gmail.com>
2020-05-23 16:02:38 +0700

    update source

--- python_architect_orelly/chapter1_ddd.py
+++ python_architect_orelly/chapter1_ddd.py
@@ -1,16 +1,49 @@
+from typing import Optional
+from dataclasses import dataclass
+from datetime import date
+
 @dataclass(frozen = True)
 class OrderLine: 
     orderid: str
     sku: str
     qty:int
 
-class Batch: 
-    def __init__(self, ref:str,sku:str,qty:int,eto:Optional[date]):
+# class Batch: 
+#     def __init__(self, ref:str,sku:str,qty:int,eto:Optional[date]):
+#         self.reference = ref
+#         self.sku = sku
+#         self.eta = enumerate
+#         self.available_quantity = qty
+
+#     def allocate(self,line:OrderLine):
+#         self.available_quantity -= line.qty
+
+class Batch:
+    def __init__(
+        self, ref: str, sku: str, qty: int, eta: Optional[date]
+    ):
         self.reference = ref
         self.sku = sku
-        self.eta = enumerate
-        self.available_quantity = qty
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
 
-    def allocate(self,line:OrderLine):
-        self.available_quantity -= line.qty
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
 
