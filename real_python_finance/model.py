02f35049b81522c35132712ec5e7492c26b304d8
anhlt003 <anhlt003@gmail.com>
2020-05-25 18:11:36 +0700

    sql alchemy

--- real_python_finance/model.py
+++ real_python_finance/model.py
@@ -3,11 +3,11 @@ from dataclasses import dataclass
 from datetime import date , datetime
 from typing import Optional, List, Set
 
-class OutOfAccount(Exception):
+class RunOutAccount(Exception):
     pass
 
 @dataclass(frozen=True)
-class OrderLine:
+class OrderUnit:
     order_id: str
     order_name : str
     order_start_time: datetime
@@ -19,12 +19,12 @@ class OrderLine:
     order_takeprofit: float
 
 class Batch: 
-    def __init__(self, batch_id: str, batch_unit_name: str, batch_total_money: int, batch_time: Optional[date]):
-        self.batch_id = batch_id
+    def __init__(self, batch_unit_id: str, batch_unit_name: str, batch_order_money: int, batch_unit_time: Optional[date]):
+        self.batch_unit_id = batch_unit_id
         self.batch_unit_name = batch_unit_name
-        self.batch_time = batch_time
-        self._purchased_money = batch_total_money
-        self._allocations = set()  
+        self.batch_unit_time = batch_unit_time
+        self._ordered_money = batch_order_money #can changed properties
+        self._ordered_allocations = set()  #list of order have to be set
     
     def call_set_order(self) -> int :
         return sum()
